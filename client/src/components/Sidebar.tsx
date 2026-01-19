3defc49eb92d42073a8105b03c90258d544d2e39
danhirst1 <43000315-danhirst1@users.noreply.replit.com>
2026-01-19 21:54:21 +0000

    Restored to 'e4b878de22693a9b4b66d473e6a4f1b7c072eb09'
    Replit-Restored-To: e4b878de22693a9b4b66d473e6a4f1b7c072eb09

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -47,7 +47,7 @@ export default function Sidebar({ activeSection, setActiveSection }: SidebarProp
         >
           {/* Light mode: white inverted logo, Dark mode: teal rose logo */}
           <img src={logoImage} alt="Acclaim Logo" className="w-8 h-8 mr-3 filter brightness-0 invert dark:hidden" />
-          <img src={acclaimRoseLogo} alt="Acclaim Logo" className="w-10 h-10 mr-3 hidden dark:block filter brightness-0 invert" />
+          <img src={acclaimRoseLogo} alt="Acclaim Logo" className="w-10 h-10 mr-3 hidden dark:block" />
           <div className="text-white">
             <div className="text-lg font-bold ml-[0px] mr-[0px] text-left">Acclaim</div>
             <div className="text-xs opacity-80 text-left">Credit Management & Recovery</div>
